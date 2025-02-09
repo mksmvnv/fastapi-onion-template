62b160581d121eb3dd47d2b2e53842b0b10abcdd
Maksim <mksmvnv@icloud.com>
2025-02-09 16:11:35 +0300

    Fix some bugs

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
 
     model_config = SettingsConfigDict(
-        env_file=".env"
+        env_file=".env", env_file_encoding="utf-8"
     )
 
 
--- backend/database.py
+++ backend/database.py
@@ -4,4 +4,7 @@ from config import settings
 
 
 async_engine = create_async_engine(url=settings.DATABASE_URL)
-async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+async_session_maker = async_sessionmaker(
+    bind=async_engine, autoflush=False, expire_on_commit=False
+)
