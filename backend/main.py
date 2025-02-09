d28333daca430f329085a4595cc66f81bdfd9668
Maksim <mksmvnv@icloud.com>
2025-02-09 16:12:12 +0300

    Fix some bugs

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,20 @@
+import uvicorn
+
 from fastapi import FastAPI
 
+from routers.v1 import all_routers
+from utils.constants import TITLE, VERSION, API_PREFIX
 
-app = FastAPI(title="FastAPI onion template", version="0.1.0")
+
+app = FastAPI(
+    title=TITLE,
+    version=VERSION,
+    root_path=API_PREFIX,
+)
+
+for router_v1 in all_routers:
+    app.include_router(router_v1)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", port=8005, reload=True)
