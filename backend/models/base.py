e9b714608984f0e2b9f9cc7f43bd11389341c8e0
Maksim <mksmvnv@icloud.com>
2025-02-09 16:12:47 +0300

    Fix some bugs

--- backend/models/base.py
+++ backend/models/base.py
@@ -2,19 +2,23 @@ from datetime import datetime
 
 from sqlalchemy import Integer, func
 from sqlalchemy.orm import (
-    DeclarativeBase, declared_attr, Mapped, mapped_column
+    DeclarativeBase,
+    declared_attr,
+    Mapped,
+    mapped_column,
 )
-from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
-class BaseModel(AsyncAttrs, DeclarativeBase):
+class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(
-        Integer, primary_key=True, autoincrement=True)
+        Integer, primary_key=True, autoincrement=True
+    )
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), onupdate=func.now())
+        server_default=func.now(), onupdate=func.now()
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
